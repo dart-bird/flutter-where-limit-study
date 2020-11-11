c11401530e3e81aae724a7400c3b9ce97cd272ff
Jinwoong <okokrt@gmail.com>
2020-11-11 23:56:17 +0900

    :confetti_ball: Update memory app more beautiful :confetti_ball:
    Added widget list
    1. Refresh
    2. Animation
    3. Image ( no data box )

--- memory_status/lib/screens/home_screen.dart
+++ memory_status/lib/screens/home_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:memory_status/components/loading_box.dart';
 import 'package:memory_status/components/memory_box.dart';
+import 'package:memory_status/components/nodata_box.dart';
 import 'package:memory_status/models/memory.dart';
 import 'package:memory_status/services/memoryQuery.dart';
+import 'package:memory_status/services/networking.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,53 +15,87 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   MemoryQuery _memoryQuery = MemoryQuery();
+  RefreshController _refreshController = RefreshController(initialRefresh: false);
+  Future<List<Memory>> _getMemoryPriceData;
+  Widget child;
+  void _onRefresh() async {
+    await Future.delayed(Duration(milliseconds: 500));
+    _fetchData();
+    _refreshController.refreshCompleted();
+  }
+
+  void _fetchData() async {
+    setState(() {
+      _getMemoryPriceData = _memoryQuery.getMemoryPriceData();
+      child = null;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              margin: EdgeInsets.all(16.0),
-              child: Text(
-                '현재 메모리 가격이예요',
-                style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
+        child: SmartRefresher(
+          controller: _refreshController,
+          onRefresh: _onRefresh,
+          header: WaterDropMaterialHeader(
+            backgroundColor: Colors.black,
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                margin: EdgeInsets.all(16.0),
+                child: Text(
+                  'Korea Memory Price',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: FutureBuilder(
-                future: _memoryQuery.getMemoryPriceData(),
-                builder: (context, AsyncSnapshot<List<Memory>> snapshot) {
-                  if (snapshot.hasError) {
-                    print(snapshot.error);
-                    return Text('정보를 가져오는데 실패했어요.');
-                  }
-                  if (snapshot.hasData == false) {
-                    return Text('데이터가 없어요.');
-                  }
-                  final _data = snapshot.data;
-                  return ListView.builder(
-                    padding: const EdgeInsets.all(8.0),
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return MemoryBox(
-                        name: _data[index].name,
-                        value: _data[index].value,
-                        unit: _data[index].unit,
-                        imgUrl: _data[index].imgUrl,
+              Expanded(
+                child: FutureBuilder(
+                  future: _getMemoryPriceData,
+                  builder: (context, AsyncSnapshot<List<Memory>> snapshot) {
+                    final _data = snapshot.data;
+
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      child = LoadingBox();
+                    } else if (snapshot.connectionState == ConnectionState.done) {
+                      if (_data.length == 0) return NoDataBox();
+                      child = ListView(
+                        children: List.generate(
+                          snapshot.data.length,
+                          (index) => MemoryBox(
+                            key: ValueKey(index),
+                            name: _data[index].name,
+                            value: _data[index].value,
+                            unit: _data[index].unit,
+                            imgUrl: _data[index].imgUrl,
+                            index: index,
+                          ),
+                        ),
                       );
-                    },
-                  );
-                },
+                    } else {
+                      child = NoDataBox();
+                    }
+                    return child;
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
